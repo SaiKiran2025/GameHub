45c1d58842e938ea3df4e845357e420899d68739
Kiran <chinnusamala@gmail.com>
2023-11-01 12:47:05 +0530

    Get Optimized Images

---
+++ src/services/image-url.ts
@@ -0,0 +1,8 @@
+//optimize high resolution images keeping in mind of slow internet connections
+
+const getCroppedImageUrl = (url: string) => {
+  const index = url.indexOf("media/") + "media/".length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
