b7f84d32a511fd43f313bf1595b1322336337de4
Kiran <chinnusamala@gmail.com>
2023-11-04 12:23:49 +0530

    Sort games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
 //optimize high resolution images keeping in mind of slow internet connections
 
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return "";
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
